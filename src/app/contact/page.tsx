cb89ed6f252f5cfbde20118c10302a165762da0d
Azhar-lone <107094194+Azhar-lone@users.noreply.github.com>
2025-04-28 22:07:44 +0500

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,7 +5,10 @@ import Hero from "./_sections/hero";
 import ContactForm from "./_sections/contact-info-form";
 import Map from "./_sections/map";
 import FAQs from "./_sections/faqs";
-
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Contact us",
+};
 export default function ContactPage() {
   return (
     <div>
